96c0b66d1240f845d3bbbf58757e20ef6aab1296
Mikhail Khotko <mkhotko@plgn.ru>
2018-08-20 18:18:58 +0500

    Add snmp manager wrapper module

--- discover/src/discover_sup.erl
+++ discover/src/discover_sup.erl
@@ -28,7 +28,13 @@ start_link() ->
 
 %% Child :: {Id,StartFunc,Restart,Shutdown,Type,Modules}
 init([]) ->
-    {ok, { {one_for_all, 0, 1}, []} }.
+    ChildSpecs = [{discover_snmp_manager,
+                   {discover_snmp_manager, start_link, []},
+                   permanent,
+                   5000,
+                   worker,
+                   [discover_snmp_manager]}],
+    {ok, { {one_for_all, 0, 1}, ChildSpecs} }.
 
 %%====================================================================
 %% Internal functions
